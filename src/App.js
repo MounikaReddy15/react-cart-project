7cd2ac5646793eafd6b2985c2353551912562ebd
unknown <mounikareddygonae15@gmail.com>
2020-07-24 15:51:24 +0530

    get read and add the data from firebase

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 // import CartItem from './CartItem';
 import Cart from './Cart';
 import Navbar from './Navbar';
+import * as firebase from 'firebase';
 
 // function App() {
 // making it a class compo
@@ -17,29 +18,83 @@ class App extends React.Component {
   this.state = {
       // here cart will have a list of products
       products: [
-          {
-              price: 99,
-              title: 'Watch',
-              qty: 1,
-              image: 'https://images.unsplash.com/photo-1525342306245-c6a1e32087ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
-              id:1
-          },
-          {
-              price: 999,
-              title: 'Mobile Phone',
-              qty: 10,
-              image: 'https://images.unsplash.com/photo-1512499617640-c74ae3a79d37?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=666&q=80',
-              id:2
-          },
-          {
-              price: 999,
-              title: 'Laptop',
-              qty: 4,
-              image: 'https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80',
-              id:3
-          }
-      ]
+          // {
+          //     price: 99,
+          //     title: 'Watch',
+          //     qty: 1,
+          //     image: 'https://images.unsplash.com/photo-1525342306245-c6a1e32087ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+          //     id:1
+          // },
+          // {
+          //     price: 999,
+          //     title: 'Mobile Phone',
+          //     qty: 10,
+          //     image: 'https://images.unsplash.com/photo-1512499617640-c74ae3a79d37?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=666&q=80',
+          //     id:2
+          // },
+          // {
+          //     price: 999,
+          //     title: 'Laptop',
+          //     qty: 4,
+          //     image: 'https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80',
+          //     id:3
+          // }
+      ],
+      loading: true
   }
+  // instead of calling mutilple times in the func
+  this.db = firebase.firestore();
+}
+
+componentDidMount() {
+  // method chaining
+  //  firebase
+  //  .firestore()
+  //  .collection('products')
+  //  //this returns a promise  
+  //  .get()
+  //  //snapshot gives snapshot of that database at that particular time  
+  //  .then((snapshot) => {
+  //    console.log(snapshot);
+  //    //doc property which is an arr in snapshot is of product collection  
+  //    snapshot.docs.map((doc) => {
+  //      //data func which retrievs all the fields in the doc as an obj
+  //      console.log(doc.data())
+  //    });
+  //    //when we fetch the prods we will setS, brow will re-render and display all the prods
+  //     const products = snapshot.docs.map((doc) => {
+  //       const data = doc.data();
+  //       data['id'] = doc.id
+  //       return data;
+  //     }) 
+  //      this.setState({
+  //        products,
+  //        loading: false
+  //      }) 
+  //  })
+
+  // firebase
+  //  .firestore()
+  this.db
+   .collection('products')
+    // which fb provides, it is called whenever something changes in products coll in fb
+   .onSnapshot((snapshot) => {
+     
+     console.log(snapshot);
+     
+     snapshot.docs.map((doc) => {
+        console.log(doc.data())
+     });
+      const products = snapshot.docs.map((doc) => {
+        const data = doc.data();
+        data['id'] = doc.id
+        return data;
+      }) 
+       this.setState({
+         products,
+         loading: false
+       }) 
+   })
 }
 
 // product - which product qty we want to inc
@@ -108,19 +163,41 @@ getCartTotal = () => {
 
   return cartTotal;
 }
+
+addProduct = () => {
+this.db
+ .collection('products')
+ .add({
+   image: '',
+   price:900,
+   qty:3,
+   title: 'Washing Machine'
+ })
+  // this will be converted into doc and added to our coll prods
+ .then((docRef) => {
+  console.log('Product has been added', docRef);
+ })
+ .catch((err) => {
+  console.log('Error:', err);
+ })
+}
+
   render () {
     // getting products from state
-    const {products} = this.state;
+    const {products, loading} = this.state;
   return (
     <div className="App">
       {/* passing a prop count */}
       <Navbar count = {this.getCartCount()}/>
+      <button onClick = {this.addProduct} style={{padding:20, fontSize:20}}> Add a Product</button>
       <Cart
       products = {products}
       onIncreaseQuantity = {this.handleIncreaseQuantity}
       onDecreaseQuantity = {this.handleDecreaseQuantity}
       onDeleteProduct    = {this.handleDeleteProduct}
       />
+      {/* using conditional rendering */}
+      {loading && <h1>Loading products...</h1>}
       <div style = {{padding: 10, fontSize: 20}}> TOTAL: {this.getCartTotal()} </div>
     </div>
   );
